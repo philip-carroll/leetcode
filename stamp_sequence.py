dcab2c98babd4f81dfc6d13ce6e0da8b40562d93
Philip Carroll <philipjudecarroll@gmail.com>
2022-07-07 21:27:45 +0100

    Working version for all known test cases

--- stamp_sequence.py
+++ stamp_sequence.py
@@ -1,46 +1,30 @@
+from collections import defaultdict
+
+
 class Solution(object):
     def findSubstrings(self, stamp, target):
-        res = {}
+        res = defaultdict(lambda: defaultdict(list))
 
         # first find all substrings
         for i in range(len(stamp)):
             for j in range(1, len(stamp) + 1):
-                if j > i:
+                if i < j:
                     s = stamp[i:j]
                     idx = {it: (i, s) for it in range(len(target)) if target.startswith(s, it)}
-                    if len(s) not in res:
-                        res[len(s)] = {}
                     for k, v in idx.items():
-                        # TODO: figure out how to find the right instance
                         # ensure the stamp won't spill over the start or end of the target
                         if (len(stamp) + k - v[0]) <= len(target) and k - v[0] >= 0:
-                            # retain the first instance of the substring
-                            if k not in res[len(s)]:
-                                res[len(s)][k] = [v]
-                            else:
-                                res[len(s)][k].append(v)
+                            # retain list of instances of the substring
+                            res[len(s)][k].append(v)
         print('0: %s' % res)
 
         # seed rem with ????
-        rem = {}
+        rem = defaultdict(lambda: defaultdict(list))
         for ik, iv in res.items():
             for jk, jv in iv.items():
-                if ik not in rem:
-                    rem[ik] = {}
-                # rem[ik][jk] = (res[ik][jk][0], '?' * len(res[ik][jk][1]))
                 rem[ik][jk] = [(r[0], '?' * len(r[1])) for r in res[ik][jk]]
 
         # then strip out substrings that completely overlap single substrings of greater length
-        # for i in sorted(res.keys()):
-        #     for ii, (ix, iv) in res[i].items():
-        #         for j in sorted(res.keys()):
-        #             for ji, (jx, jv) in res[j].items():
-        #                 # is i longer than j and j contained within i?
-        #                 if i > j and (ji in range(ii, ii + len(iv))) and (
-        #                         ji + len(jv) - 1 in range(ii, ii + len(iv))):
-        #                     rem[j][ji] = (jx, jv)
-        # print('1: %s' % rem)
-
         for i in sorted(res.keys()):
             for ii, il in res[i].items():
                 for (ix, iv) in il:
@@ -50,7 +34,7 @@ class Solution(object):
                                 # is i longer than j and j contained within i?
                                 if i > j and (ji in range(ii, ii + len(iv))) and (
                                         ji + len(jv) - 1 in range(ii, ii + len(iv))):
-                                    rem[j][ji] = (jx, jv)
+                                    rem[j][ji].append((jx, jv))
         print('1: %s' % rem)
 
         # then strip out substrings that completely overlap multiple substrings of greater length
@@ -80,27 +64,25 @@ class Solution(object):
                                         rem[j][ji] = (jx, 'x')
                                         rem[j - overlap][ji] = (jx + overlap, s)
                                         print('E: %s' % rem)
-                            i = i + 1
+                                i = i + 1
 
         for ik, iv in rem.items():
-            for jk, jv in iv.items():
-                if '?' not in jv[1]:
-                    del res[ik][jk]
-                    if not res[ik]:
-                        del res[ik]
-                else:
-                    if jv[1] != len(jv[1]) * '?':
-                        # is j at start and last character is ?
-                        if jk == 0 and jv[0] == 0 and jv[1][-1] == '?':
-                            res[ik][jk] = (jv[0], jv[1][:-1])
-                        # is j at end and first character is ?
-                        elif jk + len(jv[1]) == len(target) and jv[1][0] == '?':
-                            res[ik][jk + len(jv[1][:1])] = (jv[0] + len(jv[1][:1]), jv[1][1:])
-                            del res[ik][jk]
-                        else:
-                            del res[ik][jk]
-                            if not res[ik]:
-                                del res[ik]
+            for jk, jl in iv.items():
+                for jv in jl:
+                    if '?' not in jv[1]:
+                        if ik in res and jv in res[ik][jk]:
+                            res[ik][jk].remove(jv)
+                    else:
+                        if jv[1] != len(jv[1]) * '?':
+                            # is j at start and last character is ?
+                            if jk == 0 and jv[0] == 0 and jv[1][-1] == '?':
+                                res[ik][jk] = (jv[0], jv[1][:-1])
+                            # is j at end and first character is ?
+                            elif jk + len(jv[1]) == len(target) and jv[1][0] == '?':
+                                res[ik][jk + len(jv[1][:1])] = (jv[0] + len(jv[1][:1]), jv[1][1:])
+                                del res[ik][jk]
+                            else:
+                                del res[ik][jk]
 
         return res
 
@@ -120,9 +102,9 @@ class Solution(object):
 
         l = []
         for ik, iv in substrings.items():
-            for jk, jv in iv.items():
-                l.append((jk,) + jv)
-                # l.append((14, 4, 'a'))
+            for jk, jl in iv.items():
+                for jv in jl:
+                    l.append((jk,) + jv)
         print('L0: %s' % l)
 
         n = len(l)
@@ -140,8 +122,8 @@ class Solution(object):
                         secondunique = list(range(l[i][0], l[i][0] + len(l[i][2])))
                         second = list(range(l[i][0] - l[i][1], l[i][0] - l[i][1] + len(stamp)))
 
-                    if (firstunique[0] in second and first[0] > secondunique[0]) or (
-                            firstunique[-1] in second and first[-1] < secondunique[-1]):
+                    if (firstunique[0] in second and secondunique[-1] < firstunique[0] and first[0] > second[0]) or (
+                            firstunique[-1] in second and firstunique[-1] < secondunique[0] and first[0] < second[0]):
                         lj = l[j]
                         l[j] = l[i]
                         l.remove(l[i])
@@ -191,22 +173,23 @@ if __name__ == '__main__':
 
     # print(sol.findSubstrings('abca', 'aabcaca'))
 
-    # print(sol.movesToStamp('oz', 'ooozz'))
-    # print(sol.movesToStamp('cab', 'cabbb'))
-    # print(sol.movesToStamp('abca', 'aabcaca'))
-    # print(sol.movesToStamp('e', 'eeeeeeeeee'))
-    # print(sol.movesToStamp('uskh', 'uskhkhhskh'))
-    # print(sol.movesToStamp('aaca', 'aaaacaaaca'))  # has overlapping strings that need to be wiped
-    # print(sol.movesToStamp('ffebb', 'fffeffebbb'))  # has overlapping strings that need to be sub-wiped
-    # print(sol.movesToStamp('k', 'kkkkkkkkkkkkkkk'))
-    # print(sol.movesToStamp('ffc', 'ffffcfffffffffc'))
-    # print(sol.movesToStamp('dfe', 'ddfdfedfdfeefee'))
-    # print(sol.movesToStamp('babefafaea', 'bababefafaeaaea'))
-    # print(sol.movesToStamp('babe', 'babeabeabebabbbabeee'))
-    # print(sol.movesToStamp('uzavnaucpu', 'uzuzuzavnaucpuu'))
-    # print(sol.movesToStamp('febe', 'febeebfebebeefebebee'))
-    # print(sol.movesToStamp('bfedb', 'bfedbfedbdbfedbdbbdb'))
+    print(sol.movesToStamp('oz', 'ooozz'))
+    print(sol.movesToStamp('cab', 'cabbb'))
+    print(sol.movesToStamp('abca', 'aabcaca'))
+    print(sol.movesToStamp('e', 'eeeeeeeeee'))
+    print(sol.movesToStamp('uskh', 'uskhkhhskh'))
+    print(sol.movesToStamp('aaca', 'aaaacaaaca'))  # has overlapping strings that need to be wiped
+    print(sol.movesToStamp('ffebb', 'fffeffebbb'))  # has overlapping strings that need to be sub-wiped
+    print(sol.movesToStamp('k', 'kkkkkkkkkkkkkkk'))
+    print(sol.movesToStamp('ffc', 'ffffcfffffffffc'))
+    print(sol.movesToStamp('dfe', 'ddfdfedfdfeefee'))
+    print(sol.movesToStamp('babefafaea', 'bababefafaeaaea'))
+    print(sol.movesToStamp('babe', 'babeabeabebabbbabeee'))  # fail
+    print(sol.movesToStamp('uzavnaucpu', 'uzuzuzavnaucpuu'))
+    print(sol.movesToStamp('febe', 'febeebfebebeefebebee'))  # fail
+    print(sol.movesToStamp('bfedb', 'bfedbfedbdbfedbdbbdb'))
     print(sol.movesToStamp('abeba', 'aabebabebabebaaebaaa'))
+    print(sol.movesToStamp('scpkdxyxnwfhxpf', 'scpkdxyxnwfhxpfffpff'))
 
     # stamp = 'uzavnaucpu'
     # target = 'uzuzuzavnaucpuu'
